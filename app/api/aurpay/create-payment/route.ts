73860189b7bc39475ab60a10058c607abc30b48d
PugPep <Pugpep99@gmail.com>
2026-06-02 15:46:00 -0400

    PPWV2.221

--- app/api/aurpay/create-payment/route.ts
+++ app/api/aurpay/create-payment/route.ts
@@ -35,7 +35,7 @@ export async function POST(request: Request) {
   price: Number(total),
   currency: "USD",
   id: String(orderNumber),
-  callback_url: `${siteUrl}/api/aurpay/webhook`,
+ 
       
       
 
