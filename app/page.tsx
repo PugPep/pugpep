1e1fcacd63b078b7e8a284b198887d2e7a68e522
PugPep <Pugpep99@gmail.com>
2026-07-28 22:50:56 -0400

    PPWV1.1

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,13 @@ const [isMobile, setIsMobile] = useState<boolean | null>(null);
       {!ageVerified && (
         <div style={overlay}>
           <div style={modal}>
-            <Image src="pugpep-logo.png" alt="PUGPEP" width={150} height={150} />
+           <Image
+  src="/pugpep-logo.png"
+  alt="PUGPEP"
+  width={150}
+  height={150}
+  priority
+/>
 
             <h1 style={{ color: "#ff45d8" }}>PUGPEP Disclaimer</h1>
 
